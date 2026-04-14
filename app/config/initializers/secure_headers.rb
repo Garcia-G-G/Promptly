47a82983934bbbb11350124916435497689bbaa8
Gilber <gilbergarciata@gmail.com>
2026-04-14 15:03:10 -0600

    chore: update secure_headers config with proper CSP for Hotwire

--- app/config/initializers/secure_headers.rb
+++ app/config/initializers/secure_headers.rb
@@ -1,21 +1,20 @@
 SecureHeaders::Configuration.default do |config|
-  config.x_frame_options = "SAMEORIGIN"
+  config.hsts = "max-age=631138519; includeSubDomains"
+  config.x_frame_options = "DENY"
   config.x_content_type_options = "nosniff"
-  config.x_xss_protection = "1; mode=block"
-  config.x_download_options = "noopen"
+  config.x_xss_protection = "0"
   config.x_permitted_cross_domain_policies = "none"
   config.referrer_policy = %w[strict-origin-when-cross-origin]
+
   config.csp = {
     default_src: %w['self'],
-    font_src: %w['self' data:],
-    img_src: %w['self' data: https:],
-    object_src: %w['none'],
-    script_src: %w['self'],
+    script_src: %w['self' 'unsafe-inline'],
     style_src: %w['self' 'unsafe-inline'],
-    connect_src: %w['self'],
-    form_action: %w['self'],
+    connect_src: %w['self' ws://localhost:* wss://localhost:*],
+    img_src: %w['self' data:],
+    font_src: %w['self'],
     frame_ancestors: %w['none'],
+    form_action: %w['self'],
     base_uri: %w['self']
   }
-  config.hsts = "max-age=#{1.year.to_i}; includeSubDomains"
 end
